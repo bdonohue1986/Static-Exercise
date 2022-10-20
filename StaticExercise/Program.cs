47ccc37e753bbe5e2a6eb00a3ce131c56299d30a
bdonohue1986 <100311620+bdonohue1986@users.noreply.github.com>
2022-10-20 14:41:53 -0400

    anotherone today

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celsius = TempCoverter.CelsiusToFahrenheit(34);
+            var farenhiet = TempCoverter.FahrenheitToCelsius(74);
+
+            Console.WriteLine($"After conversions the temp you gave for Farenheit was converted to { farenhiet} Celsius and the number u provided for Celsius was converted to { celsius} Farenheit.");
+
+           
+
         }
     }
 }
---
+++ StaticExercise/TempCoverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    internal class TempCoverter 
+    {
+      public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var temp = (fahrenheit - 32) * .55;
+            return temp;
+        }
+     
+      public static double CelsiusToFahrenheit(double celsius) 
+        {
+            var temp = (celsius * 1.8) + 32;
+
+            return temp;
+        }
+
+      
+        
+
+    }
+}
